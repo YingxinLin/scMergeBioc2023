bf63ca16fa2fdfdb08c7325904aaf3d5edaba383
yingxinlin <yingxinlindsay@gmail.com>
2023-08-04 11:48:42 -0400

    update tutorial

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' COVID-19 data
+#'
+#' @description
+#' This is a filtered single-cell dataset of COVID-19 patient from the
+#' Stephenson et al 2021 dataset.
+#'
+#'
+#' @usage data(sce_covid)
+#'
+#' @format sce_covid a SingleCellExperiment object
+#' @aliases
+#' sce_covid
+#'
+#' @references Stephenson, E., Reynolds, G., Botting, R.A. et al. Single-cell multi-omics analysis of the immune response in COVID-19. Nat Med 27, 904–916 (2021). https://doi.org/10.1038/s41591-021-01329-2
+
+"sce_covid"
